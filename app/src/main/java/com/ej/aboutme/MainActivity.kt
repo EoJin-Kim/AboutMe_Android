f11548a1a2ac7d49d58449f1ee5098c5fca19981
EoJin-Kim <linexy@naver.com>
2022-07-18 17:32:06 +0900

    Feat: Bottom Navigation Bar 적용

--- app/src/main/java/com/ej/aboutme/MainActivity.kt
+++ app/src/main/java/com/ej/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ej.aboutme
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.view.View
 import com.ej.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        SystemClock.sleep(5000)
+        SystemClock.sleep(1000)
 
         setTheme(R.style.Theme_AboutMe)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.navigation.background = null
+        this.window?.apply {
+            this.statusBarColor = Color.TRANSPARENT
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        }
+
         setContentView(binding.root)
     }
 }
