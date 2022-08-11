18d73ae1cb80a572b9a876fb332f1e07920dc0e5
김어진 <linexy@naver.com>
2022-08-12 00:25:25 +0900

    Feat: 이미지 api 수정 및 사진 설정

--- app/src/main/java/com/ej/aboutme/util/ServerInfo.kt
+++ app/src/main/java/com/ej/aboutme/util/ServerInfo.kt
@@ -5,8 +5,8 @@ class ServerInfo {
     companion object{
         //val SERVER_URL = "https://12524385-a283-4cf8-908f-5a07fab92462.mock.pstmn.io"
         //val SERVER_URL = "https://85fa731a-7631-4d3e-abf4-aedc7dfa41d5.mock.pstmn.io"
-//        val SERVER_URL = "http://39.118.206.192:8080"
-        val SERVER_URL = "http://10.10.20.137:8080"
-        val SERVER_IMAGE = SERVER_URL+"/image/"
+        val SERVER_URL = "http://39.118.206.192:8080"
+//        val SERVER_URL = "http://10.10.20.137:8080"
+        val SERVER_IMAGE = "$SERVER_URL/image/"
     }
 }
